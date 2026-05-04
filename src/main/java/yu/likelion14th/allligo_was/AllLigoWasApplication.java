ff6969d094c919354f6a10b123bee6289121b442
JUNG CHOI <160298290+chlwjd0803@users.noreply.github.com>
2026-05-04 19:46:02 +0900

    feat : WAS 최초생성

---
+++ src/main/java/yu/likelion14th/allligo_was/AllLigoWasApplication.java
@@ -0,0 +1,13 @@
+package yu.likelion14th.allligo_was;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllLigoWasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AllLigoWasApplication.class, args);
+    }
+
+}
---
+++ src/test/java/yu/likelion14th/allligo_was/AllLigoWasApplicationTests.java
@@ -0,0 +1,13 @@
+package yu.likelion14th.allligo_was;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AllLigoWasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
