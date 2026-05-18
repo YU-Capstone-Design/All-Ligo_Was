c47761fe77d2b882742c08d1069139fd9de2ee78
jinupark0708 <jinupark0708@gmail.com>
2026-05-18 09:44:08 +0900

    테스트 컨트롤러 추가

---
+++ src/main/java/yu/likelion14th/allligo_was/TestController.java
@@ -0,0 +1,13 @@
+package yu.likelion14th.allligo_was;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/test")
+    public String test() {
+        return "OK";
+    }
+}
