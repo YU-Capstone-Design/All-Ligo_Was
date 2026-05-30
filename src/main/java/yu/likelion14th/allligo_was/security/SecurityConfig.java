9f363af12b14c1441772d0ed94323f50554914f7
JUNG CHOI <160298290+chlwjd0803@users.noreply.github.com>
2026-05-30 23:33:20 +0900

    feat : 웹 훅 시큐리티 해제

--- src/main/java/yu/likelion14th/allligo_was/security/SecurityConfig.java
+++ src/main/java/yu/likelion14th/allligo_was/security/SecurityConfig.java
@@ -36,6 +36,7 @@ public class SecurityConfig {
                 )
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(
+                                "/api/internal/**",
                                 "/api/v1/auth/**",
                                 "/swagger-ui/**",
                                 "/swagger-ui.html",
