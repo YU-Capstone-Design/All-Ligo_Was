6bffb46445cd4a37ec0d5e0797d026b97fffaeab
jinupark0708 <jinupark0708@gmail.com>
2026-06-02 18:58:24 +0900

    feat: 로그아웃 API 구현

--- src/main/java/yu/likelion14th/allligo_was/security/SecurityConfig.java
+++ src/main/java/yu/likelion14th/allligo_was/security/SecurityConfig.java
@@ -35,6 +35,8 @@ public class SecurityConfig {
                         session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 )
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/v1/auth/logout").authenticated()
+                        
                         .requestMatchers(
                                 "/api/internal/**",
                                 "/api/v1/auth/**",
