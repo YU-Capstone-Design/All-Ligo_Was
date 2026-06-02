083f38cd35cea182f06708293012519d60e3cabe
JUNG CHOI <160298290+chlwjd0803@users.noreply.github.com>
2026-06-02 16:47:43 +0900

    docs : 콘텐츠 404 에러코드 작성

--- src/main/java/yu/likelion14th/allligo_was/exception/ErrorCode.java
+++ src/main/java/yu/likelion14th/allligo_was/exception/ErrorCode.java
@@ -29,7 +29,13 @@ public enum ErrorCode {
     INVALID_LINK(HttpStatus.BAD_REQUEST, "링크가 유효하지 않습니다."),
 
     INVALID_COUPON_IMAGE_URL(HttpStatus.BAD_REQUEST, "쿠폰 이미지 URL이 유효하지 않습니다."),
-    
+
+    // 컨텐츠 관련
+    CONTENT_NOT_FOUND(HttpStatus.NOT_FOUND, "콘텐츠를 찾을 수 없습니다."),
+
+
+
+
     INVALID_REQUEST(HttpStatus.BAD_REQUEST, "잘못된 요청입니다."),
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버 내부 오류가 발생했습니다.");
 
