548c73a6513f8bc1ccab20803c767c27b5986ac3
JUNG CHOI <160298290+chlwjd0803@users.noreply.github.com>
2026-06-02 17:07:29 +0900

    docs : Content 트래킹 API Swagger 작성

--- src/main/java/yu/likelion14th/allligo_was/domains/content/controller/ContentController.java
+++ src/main/java/yu/likelion14th/allligo_was/domains/content/controller/ContentController.java
@@ -8,15 +8,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import yu.likelion14th.allligo_was.domains.content.api.ContentAPI;
 import yu.likelion14th.allligo_was.domains.content.service.ContentService;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/contents")
-public class ContentController {
+public class ContentController implements ContentAPI {
 
     private final ContentService contentService;
 
+    @Override
     @GetMapping("/track/{contentId}")
     public ResponseEntity<Void> trackAndRedirect(@PathVariable Long contentId){
         // 외부(소상공인) URL로 리다이렉트 응답 생성 (Location 헤더 + 302 Found)
