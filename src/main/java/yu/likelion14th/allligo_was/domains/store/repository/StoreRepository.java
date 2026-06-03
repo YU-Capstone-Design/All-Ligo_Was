00dfa2c50f1a9232ce639c24908dcd6f82d94231
JUNG CHOI <160298290+chlwjd0803@users.noreply.github.com>
2026-06-03 21:29:59 +0900

    feat : 상점 위치 기반 조회 JPQL

--- src/main/java/yu/likelion14th/allligo_was/domains/store/repository/StoreRepository.java
+++ src/main/java/yu/likelion14th/allligo_was/domains/store/repository/StoreRepository.java
@@ -1,12 +1,24 @@
 package yu.likelion14th.allligo_was.domains.store.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import yu.likelion14th.allligo_was.domains.store.entity.Store;
 import yu.likelion14th.allligo_was.domains.user.entity.User;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface StoreRepository extends JpaRepository<Store, Long> {
 
     Optional<Store> findByUser(User user);
+
+    @Query("select distinct s from Store s left join fetch s.coupons where s.region = :region")
+    List<Store> findAllByRegionWithCoupons(@Param("region") String region);
+
+    @Query(value = "SELECT * FROM store s " +
+                   "WHERE ST_Distance_Sphere(POINT(s.longitude, s.latitude), POINT(?2, ?1)) <= 3000 " +
+                   "ORDER BY ST_Distance_Sphere(POINT(s.longitude, s.latitude), POINT(?2, ?1)) ASC",
+           nativeQuery = true)
+    List<Store> findNearbyStores(@Param("latitude") Double latitude, @Param("longitude") Double longitude);
 }
