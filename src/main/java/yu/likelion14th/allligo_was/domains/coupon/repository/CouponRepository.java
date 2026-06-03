9cb1e139f87bf6ec8cdd2fec1bce60fd64b08fd3
JUNG CHOI <160298290+chlwjd0803@users.noreply.github.com>
2026-06-03 21:29:06 +0900

    feat : 상점 내 쿠폰 조회 JPQL

--- src/main/java/yu/likelion14th/allligo_was/domains/coupon/repository/CouponRepository.java
+++ src/main/java/yu/likelion14th/allligo_was/domains/coupon/repository/CouponRepository.java
@@ -1,6 +1,8 @@
 package yu.likelion14th.allligo_was.domains.coupon.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import yu.likelion14th.allligo_was.domains.coupon.entity.Coupon;
 import yu.likelion14th.allligo_was.domains.store.entity.Store;
 
@@ -9,4 +11,7 @@ import java.util.List;
 public interface CouponRepository extends JpaRepository<Coupon, Long> {
 
     List<Coupon> findAllByStore(Store store);
+
+    @Query("select c from Coupon c join fetch c.store where c.store in :stores")
+    List<Coupon> findAllByStoreIn(@Param("stores") List<Store> stores);
 }
