534a18d6c336f18d52b8013fc5d51f8183118667
JUNG CHOI <160298290+chlwjd0803@users.noreply.github.com>
2026-06-22 21:47:49 +0900

    feat : AWS OS 영국표준시로 인한 한국시간으로 변경

--- src/main/java/yu/likelion14th/allligo_was/AllLigoWasApplication.java
+++ src/main/java/yu/likelion14th/allligo_was/AllLigoWasApplication.java
@@ -4,9 +4,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import jakarta.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @EnableScheduling
 @SpringBootApplication
 public class AllLigoWasApplication {
+
+    @PostConstruct
+    public void started() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(AllLigoWasApplication.class, args);
