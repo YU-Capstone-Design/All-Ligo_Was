4d823073ca4393e32d8c08d328a54816ad05ae0f
nahyun <nahyun040411@naver.com>
2026-06-22 18:33:38 +0900

    feat: 쿼리 추가

--- src/main/java/yu/likelion14th/allligo_was/domains/content/repository/TagLogRepository.java
+++ src/main/java/yu/likelion14th/allligo_was/domains/content/repository/TagLogRepository.java
@@ -2,7 +2,19 @@ package yu.likelion14th.allligo_was.domains.content.repository;
 
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import yu.likelion14th.allligo_was.domains.content.entity.TagLog;
 
+import java.util.List;
+
 public interface TagLogRepository extends JpaRepository<TagLog, Long> {
+    @Modifying
+    @Query("""
+        update TagLog tl
+        set tl.promotionTag = null
+        where tl.promotionTag.tagId in :tagIds
+    """)
+    void nullifyPromotionTagByTagIds(@Param("tagIds") List<Long> tagIds);
 }
